23490f660e92ccf4a2386f970c829adee5cb2b47
yslu <469899431@qq.com>
2019-07-09 20:51:50 +0800

    上传gif图片 补充说明

--- src/main/java/com/how2java/controller/UploadController.java
+++ src/main/java/com/how2java/controller/UploadController.java
@@ -1,5 +1,6 @@
 package com.how2java.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.how2java.pojo.UploadedImageFile;
 import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.stereotype.Controller;
@@ -20,13 +21,19 @@ public class UploadController {
     @RequestMapping("/uploadImage")
     public ModelAndView upload(HttpServletRequest request, UploadedImageFile file)
             throws IllegalStateException, IOException {
+        //随机生成文件名
         String name = RandomStringUtils.randomAlphanumeric(10);
         String newFileName = name + ".jpg";
+        //生成文件路径
         File newFile = new File(request.getServletContext().getRealPath("/image"), newFileName);
+        System.out.println("测试数据 ---------->"+JSON.toJSONString(newFile));
         newFile.getParentFile().mkdirs();
+        //将上传的图片的内容拷贝给newFile    FileCopyUtils.copy(this.content, dest);
         file.getImage().transferTo(newFile);
 
+        //请求结束后跳转至showUploadedFilejsp文件 利用视图定位  补充前后缀
         ModelAndView mav = new ModelAndView("showUploadedFile");
+        //将上传对象传递给显示图片的jsp
         mav.addObject("imageName", newFileName);
         return mav;
     }
